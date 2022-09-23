f31727ba0c726328435303cf4d4789efdce673cc
Riley <increaseblue666@gmail.com>
2022-09-23 15:24:32 +0900

    home 상단 슬라이더 생성

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, {
+  Keyboard,
+  Navigation,
+  Pagination,
+  SwiperOptions,
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Keyboard]);
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  config: SwiperOptions = {
+    slidesPerView: 1.1,
+    navigation: true,
+    pagination: { clickable: true },
+    keyboard: { enabled: true },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
