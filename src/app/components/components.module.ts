7617ba9a5cc18126ba431761c6dda7bc21ed41d6
Riley <increaseblue666@gmail.com>
2022-09-23 17:00:44 +0900

    레스토랑 정보 로딩 컴포넌트 생성

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { IonicModule } from '@ionic/angular';
+import { LoadingRestaurantComponent } from './loading-restaurant/loading-restaurant.component';
 
 @NgModule({
-  declarations: [RestaurantComponent],
+  declarations: [RestaurantComponent, LoadingRestaurantComponent],
   imports: [CommonModule, IonicModule],
-  exports: [RestaurantComponent],
+  exports: [RestaurantComponent, LoadingRestaurantComponent],
 })
 export class ComponentsModule {}
