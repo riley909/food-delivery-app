a9d7eafeb00768b09409c1bc94f3a1d9dcfe7ae1
Riley <increaseblue666@gmail.com>
2022-09-26 08:56:59 +0900

    home페이지 로딩 조건 설정

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -8,47 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
   banners = [];
   restaurants = [];
+  isLoading = false;
 
   constructor() {}
 
   ngOnInit() {
-    this.banners = [
-      { banner: 'assets/imgs/1.jpg' },
-      { banner: 'assets/imgs/2.jpg' },
-      { banner: 'assets/imgs/3.jpg' },
-    ];
+    this.isLoading = true;
+    setTimeout(() => {
+      this.banners = [
+        { banner: 'assets/imgs/1.jpg' },
+        { banner: 'assets/imgs/2.jpg' },
+        { banner: 'assets/imgs/3.jpg' },
+      ];
 
-    this.restaurants = [
-      {
-        cover: 'assets/imgs/1.jpg',
-        name: 'Stayfit',
-        shortName: 'stayfit',
-        cuisines: ['Italian', 'Mexican'],
-        rating: 4,
-        deliveryTime: 25,
-        distance: 3.5,
-        price: 100,
-      },
-      {
-        cover: 'assets/imgs/2.jpg',
-        name: 'Stayfit1',
-        shortName: 'stayfit1',
-        cuisines: ['Italian', 'Mexican'],
-        rating: 5,
-        deliveryTime: 25,
-        distance: 0,
-        price: 100,
-      },
-      {
-        cover: 'assets/imgs/3.jpg',
-        name: 'Stayfit2',
-        shortName: 'stayfit2',
-        cuisines: ['Italian', 'Mexican'],
-        rating: 5,
-        deliveryTime: 25,
-        distance: 2.5,
-        price: 100,
-      },
-    ];
+      this.restaurants = [
+        {
+          cover: 'assets/imgs/1.jpg',
+          name: 'Stayfit',
+          shortName: 'stayfit',
+          cuisines: ['Italian', 'Mexican'],
+          rating: 4,
+          deliveryTime: 25,
+          distance: 3.5,
+          price: 100,
+        },
+        {
+          cover: 'assets/imgs/2.jpg',
+          name: 'Stayfit1',
+          shortName: 'stayfit1',
+          cuisines: ['Italian', 'Mexican'],
+          rating: 5,
+          deliveryTime: 25,
+          distance: 0,
+          price: 100,
+        },
+        {
+          cover: 'assets/imgs/3.jpg',
+          name: 'Stayfit2',
+          shortName: 'stayfit2',
+          cuisines: ['Italian', 'Mexican'],
+          rating: 5,
+          deliveryTime: 25,
+          distance: 2.5,
+          price: 100,
+        },
+      ];
+      this.isLoading = false;
+    }, 3000);
   }
 }
