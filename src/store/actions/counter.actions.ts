234192f0244f70ad4bce0651daaf7920f65ffaac
Riley <increaseblue666@gmail.com>
2022-10-06 16:09:37 +0900

    액션 타입 수정

--- src/store/actions/counter.actions.ts
+++ src/store/actions/counter.actions.ts
@@ -1,5 +1,24 @@
+import { Action } from '@ngrx/store';
 import { createAction } from '@ngrx/store';
 
-export const incrementAction = createAction('[Counter Component], Increment');
-export const decrementAction = createAction('[Counter Component], Decrement');
-export const resetAction = createAction('[Counter Component], Reset');
+export enum ActionTypes {
+  increment = '[Counter Component] Increment',
+  decrement = '[Counter Component], Decrement',
+  reset = '[Counter Component], Reset',
+}
+
+export class Increment implements Action {
+  readonly type = ActionTypes.increment;
+
+  // constructor(public payload: { val: number; status: string }) {}
+}
+
+export class Decrement implements Action {
+  readonly type = ActionTypes.decrement;
+}
+
+export class Reset implements Action {
+  readonly type = ActionTypes.reset;
+}
+
+export type CounterActionUnion = Increment | Decrement | Reset;
