4728e41456e5749f0dae24426326a9359edb507b
Riley <increaseblue666@gmail.com>
2022-10-12 11:09:22 +0900

    ngrx devtools 적용

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
 import { reducers } from 'src/store/reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { BookSearchEffects } from 'src/store/effects/book-search.effects';
+import { GoogleBooksService } from './services/google-books.service';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CounterComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      autoPause: true,
+    }),
+    HttpClientModule,
+    EffectsModule.forRoot([BookSearchEffects]),
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    GoogleBooksService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
