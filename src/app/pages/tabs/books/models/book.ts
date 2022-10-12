27e9c0c24a07653beba7322fe9a0477faa9e6657
Riley <increaseblue666@gmail.com>
2022-10-12 11:07:30 +0900

    book 데이터 타입 작성

---
+++ src/app/pages/tabs/books/models/book.ts
@@ -0,0 +1,38 @@
+type IndustryIdentifiers = {
+  type: string;
+  identifier: string;
+};
+
+export interface Book {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    industryIdentifiers: IndustryIdentifiers[];
+    readingModes: any;
+    pageCount: number;
+    printType: string;
+    categories: string[];
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    panelizationSummary: any;
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+    saleInfo: any;
+    accessInfo: any;
+    searchInfo: any;
+  };
+}
