358dad18635e8e1e747c90c10273d99f46832f71
Riley <increaseblue666@gmail.com>
2022-10-12 11:06:44 +0900

    books 리듀서 생성

---
+++ src/store/reducers/books.reducer.ts
@@ -0,0 +1,30 @@
+import { createFeatureSelector, createReducer, on } from '@ngrx/store';
+import { Book } from '../../app/pages/tabs/books/models/book';
+import * as BooksActions from '../actions/books.actions';
+
+export interface BooksState {
+  books: Book[];
+  error: string;
+}
+
+export const initialState: BooksState = {
+  books: [],
+  error: null,
+};
+
+export const booksReducer = createReducer<
+  BooksState,
+  BooksActions.BooksActionUnion
+>(
+  initialState,
+  on(BooksActions.searchBooksSuccessAction, (state, { books }) => ({
+    ...state,
+    books,
+  })),
+  on(BooksActions.searchBooksFailureAction, (state, { errorMsg }) => ({
+    ...state,
+    error: errorMsg,
+  }))
+);
+
+export const booksSelectFeature = createFeatureSelector<BooksState>('books');
