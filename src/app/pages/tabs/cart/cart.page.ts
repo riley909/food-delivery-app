b68cf4e27b46a9aa7c5234dc680d7bde4c7b3e20
Seokyung Kim <increaseblue666@gmail.com>
2022-11-03 13:12:09 +0900

    state test

--- src/app/pages/tabs/cart/cart.page.ts
+++ src/app/pages/tabs/cart/cart.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { createSelector, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { RootState } from 'src/store/reducers';
+import { Book } from '../books/models/book';
+import * as booksActions from '../../../../store/actions/books.actions';
+
+const selectBooks = (state: RootState) => state.books;
+const booksSelector = createSelector(selectBooks, (state) => state.books);
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +14,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  books$: Observable<Book[]>;
+  constructor(private store$: Store<RootState>) {
+    this.books$ = store$.select(booksSelector);
   }
 
+  ngOnInit() {}
 }
